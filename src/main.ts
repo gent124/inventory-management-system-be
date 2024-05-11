0d43beff54c6980799968aedfee23fb3279b658f
gent124 <gr47048@ubt-uni.net>
2024-05-11 18:30:19 +0200

    Addede allow

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://nuka-store.vercel.app',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
