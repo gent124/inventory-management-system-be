d5ec840b92b70c99dc780e84cf308d34a4f1e6d5
gent124 <gr47048@ubt-uni.net>
2024-05-13 21:39:59 +0200

    Testing deplo

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://nuka-store.vercel.app', 'http://localhost:5173'],
-    credentials: true,
-    allowedHeaders: '*',
-  });
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(4000);
 }
 bootstrap();
